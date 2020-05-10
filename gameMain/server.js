2d0c526bcf5b3afe49c0c987f64a76c47c37e26c
Eddie Pi <eddiepei2360@gmail.com>
2020-05-09 22:58:00 -0400

    Refactored Server

--- gameMain/server.js
+++ gameMain/server.js
@@ -37,46 +37,102 @@ io.on('connection', function(socket) {
 */
 
 
-var players = {}; // create empty dictionary to store players in
+// create empty dictionary to store players in
+// setters and getters for this are below
+var players = {};
+
+// returns value of x pos of player with certain id
+function getPlayerXPos(players, id) {
+  if (id in players){
+    return players[id].x_pos_player;
+  }
+}
+
+// sets value of x pos of player with certain id
+// requires that position is valid
+function setPlayerXPos(players, id, newPos) {
+  if (id in players){
+    players[id].x_pos_player = newPos;
+  }
+}
+
+// returns value of y pos of player with certain id
+function getPlayerYPos(players, id) {
+  if (id in players){
+    return players[id].y_pos_player;
+  }
+}
+
+// sets value of y pos of player with certain id
+// requires that position is valid
+function setPlayerYPos(players, id, newPos) {
+  if (id in players){
+    players[id].y_pos_player = newPos;
+  }
+}
+
+// returns velocity of player with certain id
+function getPlayerVel(players, id) {
+  if (id in players){
+    return players[id].p_vel;
+  }
+}
+
+// sets value of the velocity of player with certain id
+// requires that new velocity is valid
+function setPlayerYPos(players, id, newVel) {
+  if (id in players){
+    players[id].p_vel = newVel;
+  }
+}
+
+
 
 // handles new players and movement
 io.on('connection', function(socket) {
 
+  // when a new player connects, set the socket.id to be the key to the
+  // player object in the dictionary and initalize this dictionary value
+  // with default values
   socket.on('new player', function() {
-    // when a new player connects, set the socket.id to be the key to the
-    // player object in the dictionary and initalize this dictionary value
-    // with default values
     players[socket.id] = {
       x_pos_player: 300,
-      y_pos_player: 300
+      y_pos_player: 300,
+      p_vel : 3
     };
   });
 
-  socket.on('p_movement', function(data) {
-    // check the connect sockets id and effect its position
-    // based on the socket.emit data given from game.js
-    // the time_modification variables allows for smoother and
-    // more consistent movement as the client is not guarenteed
-    // to send data every 60th of a second
+  // check the connect sockets id and effect its position
+  // based on the socket.emit data given from game.js
+  // the time_modification variables allows for smoother and
+  // more consistent movement as the client is not guarenteed
+  // to send data every 60th of a second
+  socket.on('key input', function(data) {
+    // NOTE: socket.id is reffering to the id number of the socket which emits a signal
     var player = players[socket.id] || {};
+
     if (data.left) {
-      player.x_pos_player -= 3 * (( data.time_modification / (1000 / 60)));
+      player.x_pos_player -= player.p_vel * (( data.time_modification / (1000 / 60)));
     }
+
     if (data.up) {
-      player.y_pos_player -= 3 * (( data.time_modification / (1000 / 60)));
+      player.y_pos_player -= player.p_vel * (( data.time_modification / (1000 / 60)));
     }
+
     if (data.right) {
-      player.x_pos_player += 3 * (( data.time_modification / (1000 / 60)));
+      player.x_pos_player += player.p_vel * (( data.time_modification / (1000 / 60)));
     }
+
     if (data.down) {
-      player.y_pos_player += 3 * (( data.time_modification / (1000 / 60)));
+      player.y_pos_player += player.p_vel * (( data.time_modification / (1000 / 60)));
     }
+
   });
 });
 
 // send the new player positions to all clients
 setInterval(function() {
-  io.sockets.emit('player movement state', players);
+  io.sockets.emit('transmit', players);
 }, 1000 / 120);
 
 
--- gameMain/static/game.js
+++ gameMain/static/game.js
@@ -1,8 +1,16 @@
+/* TODO: have it such that no calculations are do by client
+         all changing of variables should be down soley by server
+         client just needs to take input and draw stuff
+*/
+
 var socket = io();
 
+//tell server new player has connected
+socket.emit('new player');
+
 // basic IO used to test if the server is able to transmit
-// how the player will move (TEST)
-var player_mov = {
+// how keys will be pressed
+var key_input = {
   up: false,
   down: false,
   left: false,
@@ -10,43 +18,40 @@ var player_mov = {
   time_modification: 1
 }
 
-// listen for movement
-// subject to change and may be moved into new folder
+// listen for certain key inputs
+// subject to change and may be moved into new file
 document.addEventListener('keydown', function(event) {
   switch (event.keyCode) {
     case 65: // A
-      player_mov.left = true;
+      key_input.left = true;
       break;
     case 87: // W
-      player_mov.up = true;
+      key_input.up = true;
       break;
     case 68: // D
-      player_mov.right = true;
+      key_input.right = true;
       break;
     case 83: // S
-      player_mov.down = true;
+      key_input.down = true;
       break;
   }
 });
 document.addEventListener('keyup', function(event) {
   switch (event.keyCode) {
     case 65: // A
-      player_mov.left = false;
+      key_input.left = false;
       break;
     case 87: // W
-      player_mov.up = false;
+      key_input.up = false;
       break;
     case 68: // D
-      player_mov.right = false;
+      key_input.right = false;
       break;
     case 83: // S
-      player_mov.down = false;
+      key_input.down = false;
       break;
   }
 });
-
-//tell server new player has connected
-socket.emit('new player');
 
 // tell the server how to move the player and keeps track of
 // keeps track of how many milis have passed and uses it to
@@ -58,7 +63,7 @@ setInterval(function() {
   var timeDifference = currentTime - lastUpdateTime;
 
   player_mov.time_modification = timeDifference;
-  socket.emit('p_movement', player_mov);
+  socket.emit('key input', key_input);
 
   lastUpdateTime = currentTime;
 }, 1000 / 60);
@@ -71,19 +76,36 @@ setInterval(function() {
          better organize your code
 */
 
+/* INTERPOLATION:
+1. have old data prestored in a specific dictionary to access
+2. get new pos from server
+3. have client draw movment of all sub frames that would appear between old and new pos
+   you might want it so sub frame position increase is not a fixed number and is instead a ratio
+4. DONE
+*/
+
+/*
+// store a temporary version of the data locally every transmission
+var localData;
+socket.on('transmit', function(players) {
+  localData = players;
+});
+*/
+
+
 //draw players (done for testing will be removed/modified in future)
 var canvas = document.getElementById('canvas');
 canvas.width = 800;
 canvas.height = 600;
 var context = canvas.getContext('2d');
-// draw the player
-socket.on('player movement state', function(players) {
+// draw the player do something with transmitted data
+  socket.on('transmit', function(players) {
   context.clearRect(0, 0, 800, 600);
   context.fillStyle = 'green';
   for (var id in players) {
     var player = players[id];
     context.beginPath();
-    context.arc(player.x_pos_player, player.y_pos_player, 10, 0, 2 * Math.PI);
+    context.arc(player.x_pos_player, player.y_pos_player, 30, 0, 2 * Math.PI);
     context.fill();
   }
 });
